fb347e8cadae0bd61cc8745b5231217fc7c1f3fe
Dhairya  Shah <dtshah24022006@gmail.com>
2024-10-07 02:48:30 +0530

    add bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { IconSparkles } from "@tabler/icons-react";
+import {
+  IconAi,
+  IconCloudUpload,
+  IconFileExport,
+  IconRobot,
+  IconSparkles,
+  IconTransformPoint,
+} from "@tabler/icons-react";
 
 export default function Home() {
   return (
@@ -8,10 +15,7 @@ export default function Home() {
       <nav className="flex justify-between items-center">
         <h1 className="text-2xl font-semibold">EaseGen</h1>
         <div className="flex items-center space-x-8">
-          <Link
-            href="/documentation"
-            className="font-medium hover:underline"
-          >
+          <Link href="/documentation" className="font-medium hover:underline">
             Documentation
           </Link>
           <Button className="font-semibold" variant="secondary" size="lg">
@@ -46,8 +50,63 @@ export default function Home() {
       </section>
 
       {/* How it works */}
-      <div className="max-w-3xl bg-primary/30 h-3 mx-auto mt-20 p-4">
-
+      <div className="max-w-5xl h-[600px] mx-auto mt-20 p-4">
+        <div className="grid grid-cols-12 grid-rows-4 gap-4 items-center w-full bg-primary/10 h-full p-4 rounded-xl">
+          <div className="col-span-4 row-span-4 bg-primary rounded-xl h-full w-full p-5 px-6 text-white relative">
+            <h3 className="text-5xl mt-2 font-semibold max-w-3xl leading-tight">
+              How it works?
+            </h3>
+            <div className="absolute bottom-8 right-3">
+              <IconRobot size={80} stroke={1} />
+            </div>
+          </div>
+          <div className="col-span-4 row-span-2 bg-primary rounded-xl h-full w-full p-5 px-6 text-white relative">
+            <h3 className="text-5xl mt-2 font-semibold max-w-3xl leading-tight">
+              1.
+            </h3>
+            <div className="inset-0 flex justify-center items-center">
+              <IconCloudUpload size={80} stroke={1} />
+            </div>
+            <h4 className="text-center font-semibold text-lg mt-4">
+              Upload your format
+            </h4>
+          </div>
+          <div className="col-span-4 row-span-3 bg-primary rounded-xl h-full w-full p-5 px-6 text-white relative">
+            <h3 className="text-5xl mt-2 font-semibold max-w-3xl leading-tight">
+              2.
+            </h3>
+            <div className="inset-0 flex justify-center items-center mt-20">
+              <IconTransformPoint size={80} stroke={1} />
+            </div>
+            <h4 className="text-center font-semibold text-lg mt-4">
+              Map Fields with respective functionality
+            </h4>
+          </div>
+          <div className="col-span-4 row-span-2 bg-primary rounded-xl h-full w-full p-5 px-6 text-white relative">
+            <h3 className="text-5xl mt-2 font-semibold max-w-3xl leading-tight">
+              3.
+            </h3>
+            <div className="inset-0 flex justify-center items-center">
+              <IconAi size={100} stroke={1} />
+            </div>
+            <h4 className="text-center font-semibold mt-4">
+              AI Code Generation and Execution
+            </h4>
+          </div>
+          <div className="col-span-4 row-span-1 bg-primary rounded-xl h-full w-full p-5 px-6 text-white flex space-x-14">
+            <h3 className="text-5xl mt-2 font-semibold max-w-3xl leading-tight">
+              4.
+            </h3>
+            <div className="inset-0 flex justify-center items-center">
+              <IconFileExport size={70} stroke={1.4} />
+            </div>
+          </div>
+          {/* <div className="col-span- row-span-3 bg-primary/30 rounded-3xl text-center h-48 w-20">Artist Name</div> */}
+          {/* <div className="col-span- row-span- bg-primary/30 rounded-3xl text-center h-48 w-20">User Name</div> */}
+          {/* <div className="col-span- row-span- bg-primary/30 rounded-3xl text-center h-48 w-20">Artist Interest</div> */}
+          {/* <div className="col-span- row-span- bg-primary/30 rounded-3xl text-center h-48 w-20">User Interset</div> */}
+          {/* <div className="col-span- row-span- bg-primary/30 rounded-3xl text-center h-48 w-20">Final Box</div> */}
+        </div>
       </div>
     </div>
   );
