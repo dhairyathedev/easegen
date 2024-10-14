9535622b37e8c97698476d0b24eaeddd4f124ce9
Dhairya  Shah <dtshah24022006@gmail.com>
2024-10-14 21:58:25 +0530

    made disclaimer responsive

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,8 +2,8 @@ import AuthForm from '@/components/auth-form'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthForm />
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-16 lg:p-24">
+        <AuthForm />
     </main>
   )
 }
